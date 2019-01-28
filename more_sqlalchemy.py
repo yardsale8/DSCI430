3de26ec7c81512787e063a42071cd49a7da54c5d
Todd Iverson <tiverson@smumn.edu>
2019-01-28 07:55:24 -0600

    Changes to lab 1

--- more_sqlalchemy.py
+++ more_sqlalchemy.py
@@ -1,4 +1,4 @@
 from functoolz import pipeable
-
-result_dict = lambda r: {n:v for n,v in zip(r.keys(), r)}
-result_dicts = pipeable(lambda rs: [result_dict(r) for r in rs])
+result_dict = lambda r: dict(zip(r.keys(), r))
+result_dicts = pipeable(lambda rs: list(map(result_dict, rs)))
+check_unique = pipeable(lambda df: [(col, df[col].is_unique) for col in df.columns])
