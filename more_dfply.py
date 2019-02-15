b57a8f9037f83c0bec3b98b808f74b56eb4f8b1c
Todd Iverson <tiverson@smumn.edu>
2019-02-15 08:12:56 -0600

    added the more_ files and baseball_2_5 db

--- more_dfply.py
+++ more_dfply.py
@@ -1,5 +1,58 @@
-from dfply import make_symbolic, pipe, symbolic_evaluation
+from dfply import make_symbolic, pipe, symbolic_evaluation, Intention
 import pandas as pd
+import numpy as np
+import re
+from string import punctuation, whitespace
+from functoolz import pipeable
+
+STARTS_WITH_DIGITS_REGEX = re.compile(r'\d+')
+PUNC_REGEX = re.compile('[{0}]'.format(re.escape(punctuation.replace('_', ''))))
+WS_REGEX = re.compile('[{0}]'.format(re.escape(whitespace)))
+
+def fix_name(name:str) -> str:
+    """ Makes a column name a proper identifier.
+    
+    This function will
+    
+    1. Strip leading/trailing whitspace
+    2. Remove all puncuation except _
+    3. Add a _ to the start of any string that start with a digit
+    """
+    strip = pipeable(lambda s: s.strip())
+    remove_punc = pipeable(lambda s: PUNC_REGEX.sub('',s))
+    fix_starting_digit = pipeable(lambda s: '_' + s if STARTS_WITH_DIGITS_REGEX.match(s) else s)
+    replace_whitespace = pipeable(lambda s: WS_REGEX.sub('_', s))
+    if name.isidentifier():
+        return name
+    else:
+        return name >> strip >> remove_punc >> fix_starting_digit >> replace_whitespace
+
+def test_fix_name() -> None:
+    good_name = 'good_name'
+    bad_name = " 1bad_name !1!\n"
+    assert fix_name(good_name).isidentifier()
+    assert fix_name(bad_name).isidentifier()
+test_fix_name()
+    
+
+@make_symbolic
+def fix_names(df):
+    """ Creates a dict of new_name:old_name pairs.
+    
+    Any name that is not an identifier (using the new .isidentifier predicate) 
+    The new names have all punctuation removed, outer whitespace removed,
+    and whitespace replaced with _.
+    """
+    return {fix_name(col):col for col in df.columns}
+
+def test_fix_names():
+    good_name = 'good_name'
+    bad_name = " 1bad_name!1!\n"
+    df = pd.DataFrame({good_name: [1,2,3],
+                       bad_name:[1,2,3]})
+    assert all(k.isidentifier() for k in fix_names(df))
+test_fix_names()
+
 
 @make_symbolic
 def to_datetime(series, infer_datetime_format=True):
@@ -20,3 +73,33 @@ def row_index_slice(df, *args):
         return df.loc[args[0]]
     else:
         return df.loc[args[0]:args[1]]
+    
+    
+@make_symbolic
+def maybe_tile(n, col):
+    if not hasattr(col, '__len__') or len(col) < n:
+        return pd.Series(np.tile(col, n)[:n])
+    else:
+        return pd.Series(col[:n])
+    
+    
+def ifelse(cond, then, else_):
+    if any(isinstance(o, Intention) for o in (cond, then, else_)):
+        def outfunc(df): 
+            cond_out = cond.evaluate(df) if isinstance(cond, Intention) else cond
+            n = len(cond_out)
+            if cond_out.all():
+                then_out = then.evaluate(df) if isinstance(then, Intention) else then
+                return maybe_tile(n, then_out)
+            if not cond_out.any():
+                else_out = else_.evaluate(df) if isinstance(else_, Intention) else else_
+                return maybe_tile(n, else_out)
+            else:
+                then_out = then.evaluate(df) if isinstance(then, Intention) else then
+                else_out = else_.evaluate(df) if isinstance(else_, Intention) else else_
+                return np.where(cond_out, maybe_tile(n, then_out), maybe_tile(n, else_out))
+        return Intention(outfunc)
+    else:
+        n = len(cond)
+        return np.where(cond, maybe_tile(n, then), maybe_tile(n, else_))
+    
--- more_pyspark.py
+++ more_pyspark.py
@@ -1,4 +1,58 @@
 import pandas as pd
 from functoolz import pipeable
+from pyspark.sql.types import StructType, StructField, IntegerType, FloatType, StringType, DateType
 
-to_pandas = pipeable(lambda rows: pd.DataFrame.from_dict([r.asDict() for r in rows]))
+DTYPES_TO_SPARK_TYPES = {'O':StringType,
+                         'i':IntegerType,
+                         'f':FloatType,
+                         'M':DateType}
+
+
+def get_spark_types(df, keys=[]):
+    sql_type = lambda name, dtype: StructField(name, 
+                                               DTYPES_TO_SPARK_TYPES[dtype.kind](),
+                                               True if name in keys else False)
+    cols_and_dtypes = lambda df: zip(df.columns, df.dtypes)
+    return StructType([sql_type(col, dtype) for col, dtype in cols_and_dtypes(df)])
+
+
+
+@pipeable
+def to_pandas(rows):
+    names = list(rows[0].asDict().keys()) # pyspark.Row keys perserve order
+    return pd.DataFrame.from_dict([r.asDict() for r in rows])[names] # reorder to original order
+
+
+col_startswith = pipeable(lambda prefix, cols: [c for c in cols if c.startswith(prefix)])
+col_endswith =   pipeable(lambda suffix, cols: [c for c in cols if c.endswith(suffix)])
+col_contains =   pipeable(lambda substr, cols: [c for c in cols if substr in c])
+
+def col_selector(col_names, from_=None, to=None, inclusive=True):
+    from_idx, to_idx = 0, len(col_names)
+    if from_:
+        from_idx = col_names.index(from_) if inclusive else col_names.index(from_) + 1
+    if to:
+        to_idx = col_names.index(to) if not inclusive else col_names.index(to) + 1
+    return  col_names[from_idx:to_idx]
+
+def get_column_list(col_names, from_=None, to=None, inclusive=True):
+    selected_cols = col_selector(col_names, from_=from_, to=to, inclusive=inclusive)
+    return  [c for c in col_names if c in selected_cols]
+
+@pipeable
+def cols_from(col, col_names, inclusive=True):
+    return  get_column_list(col_names, from_=col, inclusive=inclusive)
+    
+    
+@pipeable
+def cols_to(col, col_names, inclusive=True):
+    return  get_column_list(col_names, to=col, inclusive=inclusive)
+    
+    
+@pipeable
+def cols_between(col1, col2, col_names, inclusive=True):
+    return get_column_list(col_names, from_=col1, to=col2, inclusive=inclusive)
+
+@pipeable
+def all_but(cols_to_exclude, col_names):
+    return [c for c in col_names if c not in cols_to_exclude]
